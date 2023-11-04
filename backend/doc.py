d8c898001ab698bef706a1baedfce4a96c23c83b
Marcus Tuen Muk <tmmarcus73@gmail.com>
2023-11-04 15:18:33 -0400

    added some code for doc parsing

--- backend/doc.py
+++ backend/doc.py
@@ -1,2 +1,33 @@
-def getfolder(path):
-    pass
+import os
+
+from PyPDF2 import PdfReader
+from textract import process
+from docx import Document
+
+def getDoc(path):
+    
+    fileExtension = os.path.splitext(path)[1]
+    
+    text = ""
+
+    if fileExtension == '.pdf':
+
+        with open(path, 'rb') as f:
+            pdf = PdfReader(f)    
+            for page in pdf.pages:
+                text += page.extract_text()
+
+
+        with open('backend/test/test.txt', 'w') as f:
+            f.write(text)
+            f.close()
+
+    elif 'doc' in fileExtension:
+        f = open(path, 'rb')
+        
+
+
+if __name__ == "__main__":
+    getDoc('backend/test/Team_Presentation_Overview_Slides.pdf')
+    getDoc('backend/test/Lab 3 - Multi-threaded Socket Programming.pdf')
+    getDoc('backend/test/CalculatorStatechartToSourceCode2.docx')
