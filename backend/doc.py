4aaee47b53b6cdadb698f84e389cfd808275cb9a
Marcus Tuen Muk <tmmarcus73@gmail.com>
2023-11-04 15:39:34 -0400

    add code for pdf parsing

--- backend/doc.py
+++ backend/doc.py
@@ -18,16 +18,29 @@ def getDoc(path):
                 text += page.extract_text()
 
 
-        with open('backend/test/test.txt', 'w') as f:
-            f.write(text)
-            f.close()
+            return text
+        
 
     elif 'doc' in fileExtension:
         f = open(path, 'rb')
         
-
+        return text
 
 if __name__ == "__main__":
-    getDoc('backend/test/Team_Presentation_Overview_Slides.pdf')
-    getDoc('backend/test/Lab 3 - Multi-threaded Socket Programming.pdf')
-    getDoc('backend/test/CalculatorStatechartToSourceCode2.docx')
+    text = getDoc('backend/test/Team_Presentation_Overview_Slides.pdf')
+
+    with open('backend/test/test.txt', 'w') as f:
+            f.write(text)
+            f.close()
+
+    text = getDoc('backend/test/Lab 3 - Multi-threaded Socket Programming.pdf')
+
+    with open('backend/test/test.txt', 'w') as f:
+            f.write(text)
+            f.close()
+
+    text = getDoc('backend/test/CalculatorStatechartToSourceCode2.docx')
+
+    with open('backend/test/test.txt', 'w') as f:
+            f.write(text)
+            f.close()
