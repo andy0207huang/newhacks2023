92e97841e856eea77ccd2ebf90946abedeae2d15
Surya Sure <suryasure734@gmail.com>
2023-11-04 19:29:15 -0400

    latest

--- task-app/src/components/Calendar.jsx
+++ task-app/src/components/Calendar.jsx
@@ -3,14 +3,13 @@ import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 
 export default function Calendarjfg() {
-  const [value, onChange] = useState(new Date());
-
   return (
     <div className="calendar-container">
       <iframe
         src="https://embed.styledcalendar.com/#LkGMOk5NB7mv2ULOHIOV"
         title="Styled Calendar"
-        class="styled-calendar-container"
+        className="styled-calendar-container"
+        style={{ width: "100%", border: "none", height: "500px" }}
         data-cy="calendar-embed-iframe"
       ></iframe>
       <script
