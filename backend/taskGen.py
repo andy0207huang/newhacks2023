39e2d2567717b9342d4434d43a9452a63c8b3893
Marcus Tuen Muk <tmmarcus73@gmail.com>
2023-11-04 19:24:27 -0400

    added open ai code and prompt

--- backend/taskGen.py
+++ backend/taskGen.py
@@ -1,18 +1,57 @@
 import os
 import openai
+import doc
+import json
+import ast
+
 
 from dotenv import load_dotenv
 
-load_dotenv('backend\.env')
+load_dotenv('.env')
 
 openai.api_key = os.environ.get("API_KEY")
 
-completion = openai.ChatCompletion.create(
-  model="gpt-3.5-turbo",
-  messages=[
-    {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
-    {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
-  ]
-)
 
-print(completion.choices[0].message)
+def getTaskList(path, start, end):
+  prompt = f"Assuming the start date is {start}, and the final deadline is {end}, generate a general timeline with recommended deadlines for each task in the following text delimited by three backticks. Add a description for each task and output the whole thing as a JSON file."
+
+  document = doc.getDoc(path)
+
+  text = f"{prompt}\n```\n{document}\n```"
+
+  completion = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "user", "content": prompt}
+    ]
+  ) 
+
+  tasks =  completion.choices[0].message["content"]
+
+  tasks = tasks[:tasks.rfind('}') + 1]
+
+  tasks = ast.literal_eval(tasks)
+
+  tasksJson = json.dumps(tasks, indent=4)
+
+  return tasksJson
+
+
+if __name__ == "__main__":
+  tasks = getTaskList('./test/Team_Presentation_Overview_Slides.pdf', 'Nov. 4, 2023', 'Dec. 7, 2023')
+
+  tasks = tasks.replace("```", "").replace("\n", "").replace("\\", "").replace('json', '')
+
+  # with open('./test/output.txt', 'w') as f:
+  #   f.write(tasks)
+  #   f.close()
+
+  # tasks = ""
+
+  # with open('./test/output.txt', 'r') as f:
+  #   tasks = f.read()
+  #   f.close()
+
+  print(tasks)
+
+  
