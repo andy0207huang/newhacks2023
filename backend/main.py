82db6775e32e0599bd726a347b07bacc53a3b228
ahuang95 <andy0207huang@gmail.com>
2023-11-04 20:27:19 -0400

    fix py conflict

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,7 @@
-<<<<<<< HEAD
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 
-=======
-from flask import Flask, request, jsonify
-from flask_cors import CORS
-
 from doc import getDoc
->>>>>>> 07ee5418c0a59f15aad2114446bccfe4ffd2aeb9
 from taskGen import getTaskList
 
 app = Flask(__name__)
@@ -46,12 +40,9 @@ def backend_function():
     response.headers.add("Access-Control-Allow-Origin", "http://127.0.0.1:5173")
     return response
 
-<<<<<<< HEAD
 @app.route("/getTasks", methods=["POST"])
 def getTasks():
     pass
-=======
->>>>>>> 07ee5418c0a59f15aad2114446bccfe4ffd2aeb9
 
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
