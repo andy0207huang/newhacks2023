1fa64018fc42faa349a6a9fcc515686890bead5a
ahuang95 <andy0207huang@gmail.com>
2023-11-04 20:00:27 -0400

    fix

--- task-app/src/pages/Home.jsx
+++ task-app/src/pages/Home.jsx
@@ -1,62 +1,61 @@
 import React, { useState } from "react";
 import Calenderjfg from "../components/Calendar";
 import AuthButton from "../components/AuthButton";
-import logo from '../assets/breakject.ai-s.png';
-import { uploadFile, testBackend } from '../api';
 
+import logo from "../assets/breakject.ai-s.png";
+import { uploadFile } from "../api";
 
 const Home = () => {
-    // upload file
-    const [selectedFile, setSelectedFile] = useState(null);
+  // upload file
+  const [selectedFile, setSelectedFile] = useState(null);
 
-    const handleFileUpload = (event) => {
-        const file = event.target.files[0];
-        setSelectedFile(file);
-    };
+  const handleFileUpload = (event) => {
+    const file = event.target.files[0];
+    setSelectedFile(file);
+  };
 
-    // upload reponse
-    const [testResponse, setTestResponse] = useState('');
+  // backend test
+  const [testResponse, setTestResponse] = useState("");
 
-    const handleUploadClick = async () => {
-        if (selectedFile) {
-            try {
-                const response = await uploadFile(selectedFile);
-                console.log(response);
-                setTestResponse(response.msg);
-            } catch (error) {
-                console.error(error);
-            }
-        }
-    };
+  const handleUploadClick = async () => {
+    if (selectedFile) {
+      try {
+        const response = await uploadFile(selectedFile);
+        console.log(response);
+        // Handle the response as needed
+        setTestResponse(response.filename);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
 
-    // TEST
-    // const handleTestClick = async () => {
-    //     try {
-    //         const response = await testBackend();
-    //         setTestResponse(response);
-    //     } catch (error) {
-    //         console.error(error);
-    //     }
-    // };
+  return (
+    <div>
+      <img
+        src={logo}
+        style={{ height: "200px", width: "200px" }}
+        alt="breakject.ai Logo"
+      />
+      <h1>Welcome to breakject.ai!</h1>
+      <p>
+        Break down your projects and assignments into smaller tasks and
+        deadlines for enhanced productivity.
+      </p>
 
-    return (
-        <div>
-            <img src={logo} style={{ height: '200px', width: '200px' }} alt="breakject.ai Logo" />
-            <h1>Welcome to breakject.ai!</h1>
-            <p>Break down your projects and assignments into smaller tasks and deadlines for enhanced productivity.</p>
+      {/* g calendar integration */}
+      <AuthButton />
+      <Calenderjfg />
 
-            {/* g calendar integration */}
-            <AuthButton />
-            <Calenderjfg />
+      {/* upload */}
+      <input type="file" onChange={handleFileUpload} />
+      <button onClick={handleUploadClick} disabled={!selectedFile}>
+        Upload
+      </button>
 
-            {/* upload */}
-            <input type="file" onChange={handleFileUpload} />
-            <button onClick={handleUploadClick} disabled={!selectedFile}>Upload</button>
-
-            <p>Test Response: {testResponse}</p>
-
-        </div>
-    );
-}
+      <p>Test Response: {testResponse}</p>
+    </div>
+  );
+};
 
 export default Home;
